cb5d341dd138e210e48871f58ed009be08f6a040
andinikhrs <94823342+andinikhrs@users.noreply.github.com>
2021-12-02 20:20:36 +0700

    Update soal2.php

--- soal2.php
+++ soal2.php
@@ -2,34 +2,40 @@
     <head>
         <title> soal 2 </title>
     <head>  
-        <body align="center">
+        <body >
+            <table cellspacing="4">
+            <tr>
+                <td>Nama Bendahara</td>
+                <td>:<td>
+                    <td><input type="text" name="bendahara" required></td>
+            </tr>
 
-        <td>
-            <FORM ACTION="" METHOD="POST" NAME="INPUT">
-                <H2><b>Penggajihan Karyawan</b></H2>
-                <h1><b>PT. Selalu Makmur Jaya</b></h1>
-                Nama Anda : <input type="text" name="nama" required><br>
-                Nama Pekerja : <input type="text" name="nama" required><br>
-                <tr>
+            <tr>
+                <td>Nama Pekerja</td>
+                <td>:<td>
+                    <td><br><input type="text" name="pekerja" required></br></td>
+            </tr>
+
+            <tr>
             <td>Jenis Kelamin : </td>
             <td><input type="radio" name="Laki-laki" value="Laki-laki">Laki-laki</td>
             <td><input type="radio" name="Perempuan" value="Perempuan">Perempuan</td><br>
+            </tr>
+
+            <tr>
             <td>Tanggal Gaji : <input name="tanggal"type="date"></td></br>
+            </tr>
+
+            <tr>
             <td>Jabatan : <select name="jabatan" id=""></td>
             <td><option value="---pilih jabatan---" name="---pilih jabatan---">---pilih jabatan---</option></td>
             <td><option value="d" name="d">Direktur</option></td>
             <td><option value="m" name="m">Manager</option></td>
             <td><option value="s" name="s">Sekertaris</option></td>
             <td><option value="o" name="o">OB</option></td>
-            <td>Pendidikan Terakhir : <select name="pendidikanterakhir" id=""></td>
-            <td><option value="---pilih pendidikan---" name="---pilih pendidikan---">---pilih pendidikan---</option></td>
-            <td><option value="SD" name="SD">SD</option></td>
-            <td><option value="SMP" name="SMP">SMP</option></td>
-            <td><option value="SMA" name="SMA">SMA</option></td>
-        </tr>
-    
-        
-            </td>
-        </FORM>
+            </tr>
+
+
+</table>
     </body>
 </html>
