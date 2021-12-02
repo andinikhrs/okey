3c1cebb6c82f99e4bc16d8c3af216f7885b80231
andinikhrs <94823342+andinikhrs@users.noreply.github.com>
2021-12-02 11:40:15 +0700

    Add files via upload

---
+++ soal2.php
@@ -0,0 +1,35 @@
+<html>
+    <head>
+        <title> soal 2 </title>
+    <head>  
+        <body align="center">
+
+        <td>
+            <FORM ACTION="" METHOD="POST" NAME="INPUT">
+                <H2><b>Penggajihan Karyawan</b></H2>
+                <h1><b>PT. Selalu Makmur Jaya</b></h1>
+                Nama Anda : <input type="text" name="nama" required><br>
+                Nama Pekerja : <input type="text" name="nama" required><br>
+                <tr>
+            <td>Jenis Kelamin : </td>
+            <td><input type="radio" name="Laki-laki" value="Laki-laki">Laki-laki</td>
+            <td><input type="radio" name="Perempuan" value="Perempuan">Perempuan</td><br>
+            <td>Tanggal Gaji : <input name="tanggal"type="date"></td></br>
+            <td>Jabatan : <select name="jabatan" id=""></td>
+            <td><option value="---pilih jabatan---" name="---pilih jabatan---">---pilih jabatan---</option></td>
+            <td><option value="d" name="d">Direktur</option></td>
+            <td><option value="m" name="m">Manager</option></td>
+            <td><option value="s" name="s">Sekertaris</option></td>
+            <td><option value="o" name="o">OB</option></td>
+            <td>Pendidikan Terakhir : <select name="pendidikanterakhir" id=""></td>
+            <td><option value="---pilih pendidikan---" name="---pilih pendidikan---">---pilih pendidikan---</option></td>
+            <td><option value="SD" name="SD">SD</option></td>
+            <td><option value="SMP" name="SMP">SMP</option></td>
+            <td><option value="SMA" name="SMA">SMA</option></td>
+        </tr>
+    
+        
+            </td>
+        </FORM>
+    </body>
+</html>
---
+++ soal3.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title> soal 3 </title>
+    <head>  
+        <body align="center">
+
+        <td>
+            <FORM ACTION="" METHOD="POST" NAME="INPUT">
+                <H2><b>FORM PENGULANGAN</b></H2>
+            <li>Deret Bilangan Ganjil</li>
+            <li>Segitiga Sama Kaki Terbalik</li>
+            </td>
+            <input type="submit" name="kirim"  value="kirim">
+        </FORM>
+    </body>
+</html>
